19b4c6adda96275debfb837ada0cb9bdd71f40c5
Tim Severien <timseverien@gmail.com>
2020-02-05 13:28:07 +0100

    Add Intl.Collator vs localeCompare example

---
+++ ES2015/Intl.Collator/06-vs-localeCompare.js
@@ -0,0 +1,24 @@
+const query = 'besta';
+
+const data = [
+	'bestå',
+	'flärdfull',
+	'grönkulla',
+	'norröra',
+	'ödmjuk',
+	'smörboll',
+];
+
+const locale = 'se';
+const settings = {
+	sensitivity: 'base',
+	usage: 'search',
+};
+
+// Use String.prototype.localeCompare() for one-shot checks
+console.log(`${query} == ${data[0]} =>`, query.localeCompare(data[0], locale, settings) === 0);
+
+// Use Intl.Collator for comparing several items
+const collator = new Intl.Collator(locale, settings);
+
+console.log(data, '=>', data.filter(v => collator.compare(v, query) === 0));
