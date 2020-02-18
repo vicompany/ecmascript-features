5e8601a577e2886e6869d1dfb250cd536fbf74d7
Tim Severien <timseverien@gmail.com>
2020-02-18 11:08:01 +0100

    Add ability to filter scripts

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const chalk = require('chalk');
 const glob = require('glob');
 
+const args = process.argv.slice(2);
+
+const PATTERN_FILTER = args.length > 0 ? args[0].toLocaleLowerCase() : null;
+
 glob('ES+([0-9])/**/*.js', (error, files) => {
 	if (error) {
 		throw error;
 	}
 
+	const filesFiltered = PATTERN_FILTER
+		? files.filter(f => f.toLocaleLowerCase().includes(PATTERN_FILTER))
+		: files;
+
 	process.stdout.write('\n');
 
-	files.forEach((file, index) => {
-		process.stdout.write(chalk.blue.bold(`${file} (${1 + index}/${files.length})\n`));
+	filesFiltered.forEach((file, index) => {
+		if (PATTERN_FILTER && !file.toLocaleLowerCase().includes(PATTERN_FILTER)) {
+			return;
+		}
+
+		process.stdout.write(chalk.blue.bold(`${file} (${1 + index}/${filesFiltered.length})\n`));
 
 		// eslint-disable-next-line global-require
 		require(`./${file}`);
@@ -17,4 +29,3 @@ glob('ES+([0-9])/**/*.js', (error, files) => {
 		process.stdout.write('\n');
 	});
 });
-
