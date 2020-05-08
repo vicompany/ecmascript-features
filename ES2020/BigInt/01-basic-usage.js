c10fe129f87df1e2532c89bf9c7e1397bc5a273b
Tim Severien <timseverien@gmail.com>
2020-05-08 11:07:11 +0200

    Add BigInt example

---
+++ ES2020/BigInt/01-basic-usage.js
@@ -0,0 +1,8 @@
+module.exports = () => {
+	const a = 999999999999999999999999999999999999999999n;
+	const b = BigInt('1');
+
+	console.log('a =', a);
+	console.log('b =', b);
+	console.log('a + b > Number.MAX_SAFE_INTEGER =', a + b > Number.MAX_SAFE_INTEGER);
+};
