d667161a7939db040fb4df31977464345e21a0a3
Tim Severien <timseverien@gmail.com>
2020-06-24 11:57:23 +0200

    Add some more BigInt examples

--- ES2020/BigInt/04-reflect.js
+++ ES2020/BigInt/04-reflect.js
@@ -2,7 +2,9 @@ module.exports = () => {
 	console.log('1n instanceof BigInt =', 1n instanceof BigInt);
 	console.log('typeof 1n            =', typeof 1n, '\n');
 
+	console.log('Number.isFinite(1n)       =', Number.isFinite(1n));
 	console.log('Number.isInteger(1n)      =', Number.isInteger(1n));
+	console.log('Number.isNaN(1n)          =', Number.isNaN(1n));
 	console.log('Object.getPrototypeOf(1n) =', Object.getPrototypeOf(1n));
 	console.log('Object.keys(1n)           =', Object.keys(1n));
 };
